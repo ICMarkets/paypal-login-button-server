4a20198c641d3760cd5e41033bf4a045d6cd75fa
VladimirIC <v.fedorov@icmarkets.com.ua>
2018-10-09 13:23:29 +0300

    Issue #WEB-823 (Enable Logging for Paypal)

--- index.js
+++ index.js
@@ -17,11 +17,71 @@ function cors (req, res) {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control');
 }
 
-http.createServer((req, res) =>
-    cors(req, res) || req.url.indexOf('version') !== -1
-        ? res.writeHead(200, {'Content-Type': 'text/plain'}) || res.end(version)
-        : gateway.clientToken.generate({}, (err, _) => err
-            ? res.statusCode = 404 || res.end()
-            : res.writeHead(200, {'Content-Type': 'text/plain'}) || res.end(_.clientToken)
-        )
-).listen(80);
+// Please do not write code like this!
+//
+// http.createServer((req, res) =>
+//     cors(req, res) || req.url.indexOf('version') !== -1
+//         ? res.writeHead(200, {'Content-Type': 'text/plain'}) || res.end(version)
+//         : gateway.clientToken.generate({}, (err, _) => err
+//             ? res.statusCode = 404 || res.end()
+//             : res.writeHead(200, {'Content-Type': 'text/plain'}) || res.end(_.clientToken)
+//         )
+// ).listen(80);
+
+http.createServer(function (req, res) {
+    cors(req, res);
+    if (req.url.indexOf('version') !== -1) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(version);
+    } else if (req.url.indexOf('log?d=') !== -1) {
+        log(req, req.url.split('log?d=')[1]);
+        res.writeHead(200);
+        res.end();
+    } else {
+        gateway.clientToken.generate(function (err, success) {
+            if (err) {
+                res.statusCode = 404;
+                res.end();
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end(success.clientToken);
+            }
+        });
+    }
+}).listen(8080);
+
+function log (req, data) {
+    if (console && console.log && req && data) {
+        var dt = new Date();
+        var ms = dt.getMilliseconds();
+        var len = ms.toString().length;
+        if (len === 1) {
+            ms = '00' + ms;
+        } else if (len === 2) {
+            ms = '0' + ms;
+        }
+        var dateLog = dateFormat(dt, 'DD-MM-YYYY');
+        var timeLog = dt.toLocaleTimeString() + '.' + ms;
+
+        var info = {
+            requestIP: req.connection.remoteAddress,
+            requestHeaders: req.headers,
+            paypalData: data
+        };
+
+        var logString = dateLog + ' ' + timeLog + ' > ' + JSON.stringify(info);
+        console.log('\x1b[0m', logString);
+    }
+}
+
+function dateFormat(date, format) {
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+    format = format.replace("DD", (d < 10 ? '0' : '') + d);
+    format = format.replace("MM", (m < 9 ? '0' : '') + (m + 1));
+    format = format.replace("YYYY", y);
+
+    return format;
+}
+
